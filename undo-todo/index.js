d8587978d3961c1298103ca64a791567862775c1
Sean Chen <skypemaster007@gmail.com>
2015-12-17 13:21:49 -0800

    Add components

--- undo-todo/index.js
+++ undo-todo/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import todoApp from './reducers';
+import App from './containers/App';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './containers/App';
-import todoApp from './reducers';
 
 const store = createStore(todoApp);
 
