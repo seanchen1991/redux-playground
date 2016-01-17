1f5cf9dbab16abcad66eb576f4d71b893a76795d
Sean Chen <skypemaster007@gmail.com>
2016-01-16 19:40:18 -0800

    Finish up server implementation

--- voting-server/index.js
+++ voting-server/index.js
@@ -1,3 +1,12 @@
 import makeStore from './src/store'
+import startServer from './src/server'
+import entries from './entries.json'
 
 export const store = makeStore()
+startServer(store)
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: entries
+})
+store.dispatch({type: 'NEXT'})
